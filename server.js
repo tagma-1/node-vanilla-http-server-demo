70646ed1035ea09237f981eb510155afa4206ffe
Tagma-1 <31788490+tagma-1@users.noreply.github.com>
2017-11-22 16:20:32 +1100

    Add new requests and responses (including gif)

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const HTTP = require('http');
-const { sendJSON, sendHTML } = require('./utils')
+const { sendJSON, sendHTML, sendImage } = require('./utils')
+
 
 const server = HTTP.createServer((request, response) => {
   const path = request.url
@@ -9,20 +10,30 @@ const server = HTTP.createServer((request, response) => {
   } else if (path === '/hello') {
     response.end('Hello!');
   } else if (path === '/about') {
-    sendHTML(response, `<! doctype html>
-    <html>
-      <body>
-        <h1> About </h1>
-        <p> This is a paragraph </h1>
-      </body>
-    </html>`);
+    sendHTML(response, `
+      <h1> About </h1>
+      <p> This is a paragraph </h1>
+    `);
   } else if (path === '/postcode') {
     sendJSON(response, { "name" : "City", "postcode": 1000});
   } else if (path === '/postcode/9000') {
     sendJSON(response, { "name" : "City9000", "postcode": 9000});
+  } else if (path === '/assets/main.css') {
+    response.writeHead(200, {
+      'Content-Type': 'text/css'
+    });
+    response.end(`
+    h1 {
+      text-align: center;
+    }
+    `);     
+  } else if (path === '/assets/example.gif') {
+    sendImage(response, './assets/simpsons_gif.gif');
   } else {
-    response.writeHead(404);
-    response.end('Page not found');
+    response.writeHead(404, {
+      'Content-Type': 'application/json'
+    });
+    response.end(JSON.stringify({ '404': 'Page not found'}));
   }
 });
 
--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+const FS = require('fs');
+
 function sendJSON(response, object) {
   response.writeHead(200, {
     'Content-Type': 'application/json'
@@ -9,10 +11,33 @@ function sendHTML(response, content) {
   response.writeHead(200, {
     'Content-Type': 'text/html'
   });
-  response.end(content);
+  response.end(`
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/assets/main.css">
+    <title>Server</title>
+  </head>
+  <body>
+    ${content}
+  </body>
+  </html>
+  `);
+}
+
+function sendImage(response, image) {
+  let img = FS.readFileSync(image);
+  response.writeHead(200, {
+    'Content-Type': 'image/gif'
+  });
+  response.end(img);
 }
 
 module.exports = {
   sendJSON,
-  sendHTML
+  sendHTML,
+  sendImage
 }
